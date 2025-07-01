568d8ead7d1174f747c842cdf17a11414e988767
Michael Lin (WSL) <michaellin0902@gmail.com>
2025-07-01 13:36:39 -0700

    compile fix 5, why do I have to implement things for i_system.h...

--- linuxdoom-1.10/i_video.c
+++ linuxdoom-1.10/i_video.c
@@ -137,3 +137,45 @@ void I_ReadScreen (byte* scr) {
 
 
 
+//
+// I_StartFrame
+//
+void I_StartFrame (void)
+{
+    // er?
+}
+
+
+//
+// I_StartTic
+//
+void I_StartTic (void)
+{
+
+    // if (!X_display)
+	// return;
+
+    // while (XPending(X_display))
+	// I_GetEvent();
+
+    // // Warp the pointer back to the middle of the window
+    // //  or it will wander off - that is, the game will
+    // //  loose input focus within X11.
+    // if (grabMouse)
+    // {
+	// if (!--doPointerWarp)
+	// {
+	//     XWarpPointer( X_display,
+	// 		  None,
+	// 		  X_mainWindow,
+	// 		  0, 0,
+	// 		  0, 0,
+	// 		  X_width/2, X_height/2);
+
+	//     doPointerWarp = POINTER_WARP_COUNTDOWN;
+	// }
+    // }
+
+    // mousemoved = false;
+
+}
