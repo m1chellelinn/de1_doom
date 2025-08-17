13816376dfb7c76e793ed445281f6b9ee6aea58a
Michael Lin (WSL) <michaellin0902@gmail.com>
2025-08-16 23:44:39 -0700

    patches are flickering. experiment 1: no delay, but try to update memory after FPGA

--- linuxdoom-1.10/m_menu.c
+++ linuxdoom-1.10/m_menu.c
@@ -1855,7 +1855,8 @@ void M_Drawer (void)
         skullPtr
     );
 
-    *(screens[0]+0x57C1) = 0x49; // this was just one of the pixels written by FPGA
+    // *(screens[0]+0x57C1) = 0x49; // this was just one of the pixels written by FPGA
+    *screens[0] = 0x0; // does it work with any arbitrary pixel?
 
     // while (true) {
         HAL_I_FinishUpdate(screens[0]);
