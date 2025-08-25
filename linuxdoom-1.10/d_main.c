9cf950c29f3cd0ef163d4b0b413bc3493eb7b936
Michael Lin (WSL) <michaellin0902@gmail.com>
2025-08-24 23:09:41 -0700

    fix cpu too busy constantly waiting for menu draws

--- linuxdoom-1.10/d_main.c
+++ linuxdoom-1.10/d_main.c
@@ -198,7 +198,7 @@ void D_ProcessEvents (void)
 gamestate_t     wipegamestate = GS_DEMOSCREEN;
 extern  boolean setsizeneeded;
 extern  int             showMessages;
-// long long prev_time_bg_ms = 0;
+long long prev_time_bg_ms = 0;
 long long prev_time_text_ms = 0;
 
 void R_ExecuteSetViewSize (void);
@@ -271,12 +271,12 @@ void D_Display (void)
 
       case GS_DEMOSCREEN:
 		
-		// clock_gettime(CLOCK_MONOTONIC, &ts);
-		// current_time_ms = (ts.tv_sec << 10) + (ts.tv_nsec >> 16);
-		// if (current_time_ms - prev_time_bg_ms >= DEMO_RENDER_INTERVAL_MS) {
-		// 	prev_time_bg_ms = current_time_ms;
+		clock_gettime(CLOCK_MONOTONIC, &ts);
+		current_time_ms = (ts.tv_sec << 10) + (ts.tv_nsec >> 16);
+		if (current_time_ms - prev_time_bg_ms >= DEMO_RENDER_INTERVAL_MS) {
+			prev_time_bg_ms = current_time_ms;
 			D_PageDrawer ();
-		// }
+		}
 
 		break;
     }
